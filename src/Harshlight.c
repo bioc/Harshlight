ff207f28cfb8163bafa4ebac1b207711283e7ba2
unknown <mpellegri@rockefeller.edu>
2007-09-16 19:32:30 +0000

    Updated version number
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Harshlight@27186 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Harshlight.c
+++ src/Harshlight.c
@@ -1438,7 +1438,7 @@ void report_overall_header(char **fname, int *ext_rad, double *comp_q_br, double
   PP "%%%%EndComments\n");
   PP "%%%%Page: 1 1\n");
   PP "grestore gsave /Times-Roman findfont %d scalefont setfont\n %d %d moveto (Harshlight report: ", 12, 25, 755);
-  PP "%s) show\n %d %d moveto (Version 1.1.0) show\n", asctime(localtime(&curr_time)), 490, 755);
+  PP "%s) show\n %d %d moveto (Version 1.6.0) show\n", asctime(localtime(&curr_time)), 490, 755);
 
   PP "%d %d moveto (EXTENDED defects:) show\n", x_text2, y_text - 25);
   PP "%d %d moveto (radius of the median kernel) show\n", x_text, y_text - 40);
